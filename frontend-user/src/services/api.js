00495ca48444fa15c799650c4962b7c0576a3e14
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-16 10:10:32 +0630

    Update api.js

--- frontend-user/src/services/api.js
+++ frontend-user/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// ✅ Use environment variable; do NOT hardcode the URL here
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+//const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = 'https://testing-backend-deploy-epvl.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_URL,
