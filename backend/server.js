66a27f42acf84fd04c4cb5b02141b90603c8839f
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-16 09:09:38 +0630

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: [
-      process.env.FRONTEND_URL || 'http://localhost:3000',
+      process.env.FRONTEND_URL || 'http://localhost:3000', 'https://testing-frontend-deploy.onrender.com',
       process.env.ADMIN_URL || 'http://localhost:3001',
       process.env.SUPER_ADMIN_URL || 'http://localhost:3002',
       process.env.MAIN_ADMIN_URL || 'http://localhost:3003'
