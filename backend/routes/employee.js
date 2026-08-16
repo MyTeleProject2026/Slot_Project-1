0ea8352efd3aa167c36729564fd2effbbc516b22
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-16 08:21:56 +0630

    Update employee.js

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -7,7 +7,6 @@ const { ROLES } = require('../config/roles');
 router.use(authenticate);
 router.use(authorize(ROLES.EMPLOYEE, ROLES.ADMIN, ROLES.SUPER_ADMIN, ROLES.MAIN_ADMIN));
 
-// Employee-specific endpoints
 router.get('/users', employeeController.getUsers);
 router.get('/users/:id', employeeController.getUserDetails);
 router.put('/users/:id/status', employeeController.updateUserStatus);
