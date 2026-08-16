9fe9bc755e44b0b03d5ce6e732571afd64fbad33
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-17 00:31:52 +0630

    Update Layout.jsx

--- frontend-user/src/components/common/Layout.jsx
+++ frontend-user/src/components/common/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Component } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
