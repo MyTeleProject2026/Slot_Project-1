a95715071935ec26865934a63ee448bf2b083664
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-16 13:34:16 +0630

    Update useGames.js

--- frontend-user/src/hooks/useGames.js
+++ frontend-user/src/hooks/useGames.js
@@ -3,7 +3,27 @@ import GameContext from '../contexts/GameContext';
 
 export const useGames = () => {
   const context = useContext(GameContext);
-  if (!context) throw new Error('useGames must be used within GameProvider');
+  if (!context) {
+    return {
+      games: [],
+      providers: [],
+      categories: [],
+      loading: false,
+      favorites: [],
+      activeGame: null,
+      fetchGames: async () => [],
+      fetchProviders: async () => [],
+      searchGames: async () => [],
+      getGamesByProvider: async () => [],
+      getGamesByCategory: async () => [],
+      startGame: async () => null,
+      spin: async () => null,
+      collectWin: async () => null,
+      toggleFavorite: () => {},
+      isFavorite: () => false,
+      setActiveGame: () => {},
+    };
+  }
   return context;
 };
 
