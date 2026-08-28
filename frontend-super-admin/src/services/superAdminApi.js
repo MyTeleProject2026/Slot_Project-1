24af6baa3e5bf08b008509bb5072332f6312b3ab
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-29 04:33:41 +0630

    feat: add owner funding overview API client

--- frontend-super-admin/src/services/superAdminApi.js
+++ frontend-super-admin/src/services/superAdminApi.js
@@ -19,6 +19,7 @@ const superAdminApi = {
 
   getBalance: () => unwrap(api.get('/super-admin/balance')),
   getMasterBalance: () => unwrap(api.get('/slotopol-funding/balance')),
+  getFundingOverview: () => unwrap(api.get('/slotopol-funding/overview')),
   addBalance: (payload) => unwrap(api.post('/super-admin/balance/add', payload)),
   transferBalance: (payload) => unwrap(api.post('/super-admin/balance/transfer', payload)),
   getFundingHistory: (params) => unwrap(api.get('/slotopol-funding/history', { params })),
